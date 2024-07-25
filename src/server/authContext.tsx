b55a5113aac30a0836f918ea16a4eb30d2ccf724
ogmoises <moisesaltouniann@gmail.com>
2024-07-25 18:03:56 -0300

    1

--- src/server/authContext.tsx
+++ src/server/authContext.tsx
@@ -2,13 +2,9 @@
 import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
-interface User {
-  email: string;
-}
-
 interface AuthContextProps {
-  user: User | null;
-  login: (user: User) => void;
+  token: string | null;
+  login: (token: string) => void;
   logout: () => void;
   isLoading: boolean;
 }
@@ -16,38 +12,35 @@ interface AuthContextProps {
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [user, setUser] = useState<User | null>(null);
-    const [isLoading, setIsLoading] = useState(true);  // Estado de carregamento
-    const router = useRouter();
+  const [token, setToken] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
 
-    useEffect(() => {
-      const fetchUser = async () => {
-        const userData = localStorage.getItem('user');
-  
-        if (userData) {
-          setUser(JSON.parse(userData));
-        }
-        setIsLoading(false);
-      };
-  
-      fetchUser();
-    }, []);
+  useEffect(() => {
+    const fetchToken = async () => {
+      const savedToken = localStorage.getItem('token');
+      if (savedToken) {
+        setToken(savedToken);
+      }
+      setIsLoading(false);
+    };
+    fetchToken();
+  }, []);
 
-  const login = (userData: User) => {
-    setUser(userData);
-    console.log(JSON.stringify(userData))
-    localStorage.setItem('user', JSON.stringify(userData));
+  const login = (token: string) => {
+    setToken(token);
+    localStorage.setItem('token', token);
     router.push('/'); // Redirecionamento ao fazer login
   };
 
   const logout = () => {
-    setUser(null);
-    localStorage.removeItem('user');
-    router.push('/login'); // Redirecionamento ao fazer logout
+    setToken(null);
+    localStorage.removeItem('token');
+    router.push('/'); // Redirecionamento ao fazer logout
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout, isLoading }}>
+    <AuthContext.Provider value={{ token, login, logout, isLoading }}>
       {children}
     </AuthContext.Provider>
   );
