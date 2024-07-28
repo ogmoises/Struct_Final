8356cdae0a8f4713fc44cb875e7002131edf0905
matheusgit <matheusdesouzafernandes54@gmail.com>
2024-07-27 21:40:06 -0300

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@
 import React from 'react';
 import Header from "./_components/Header"
 import { AuthProvider } from '../server/authContext';
+import Footer from './_components/Footer';
+import MainPage from './_components/MainPage';
 
 export default function Page({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
       <Header/>
       <main>{children}</main>
+      <MainPage/>
+      <Footer/>
     </AuthProvider>
   )
 }
